02e1ba1c14e353451a49edc89e937dba061bfe15
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-07-27 17:41:04 +0200

    feat: add 6 markers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react'
 import './App.css'
-
 import MapContainer from './MapContainer'
+import List from './List'
+
+const places = [
+  {
+    title: 'Elektrenu marios',
+    name: 'elektrenu-marios',
+    position: {lat: 54.755719, lng: 24.669558}
+  },{
+    title: 'Ice Hockey Arena',
+    name: 'ice-hockey-arena',
+    position: {lat: 54.78447, lng: 24.669604}
+  },{
+    title: 'Power Plant',
+    name: 'power-plant',
+    position: {lat: 54.771311, lng: 24.648063}
+  },{
+    title: 'Main Beach',
+    name: 'main-beach',
+    position: {lat: 54.782091, lng: 24.665443}
+  },{
+    title: 'Second Beach',
+    name: 'second-beach',
+    position: {lat: 54.779074, lng: 24.677175}
+  },{
+    title: 'Elektrenu Mergeles Marijos Kankiniu Karalienes baznycia',
+    name: 'church',
+    position: {lat: 54.782097, lng: 24.680786}
+  }
+]
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <MapContainer />
+      <div className="app">
+        <MapContainer
+          places={places}
+        />
+        <List
+          places={places}
+        />
       </div>
     )
   }
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react'
 import { GoogleApiWrapper, Map, InfoWindow, Marker } from 'google-maps-react'
 
+// nullStyle used to remove styles from map, so it can be styled in my App.css
+const nullStyle = {
+  position: null,
+  left: null,
+  right: null,
+  bottom: null,
+  top: null,
+  width: null,
+  height: null
+}
+
 export class MapContainer extends Component {
+
+  // TODO: delete after finish
+  // logas = () => {
+  //   console.log(window.google)
+  // }
+  componentDidMount() {debugger}
+  dadadum = () => {debugger}
+
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map
+        className={'map'}
+        google={this.props.google}
+        zoom={13}
+        initialCenter={{lat: 54.753986, lng: 24.670219}}
+        style={nullStyle}
+      >
+        {this.props.places.map(place => (
+          <Marker
+            key={`marker-${place.name}`}
+            title={place.title}
+            name={place.name}
+            position={place.position}
+            // onClick={this.dadadum}
+          />
+        ))}
+        {this.props.places.map(place => (
+          <InfoWindow
+            key={`infowindow-${place.name}`}
+            visible={true}>
+            <div>{place.title}</div>
+          </InfoWindow>
+        ))}
       </Map>
     )
   }
