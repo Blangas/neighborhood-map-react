cdf99e9baedf77a859f2f7e969a94e8d79a64f68
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-08-03 16:39:07 +0200

    fix: add tabIndex to info-window

--- src/InfoWindowContent.js
+++ src/InfoWindowContent.js
@@ -20,18 +20,20 @@ export default class InfoWindowContent extends Component {
   render () {
     if (this.props.marker) {
       return (
-        <div className="info-window">
-          <div><strong>{this.props.marker.title}</strong></div>
-          <div>{this.props.marker.address}</div>
-          <div className="photos">
-            {this.props.photos.map((photo, i) => (
-              <img
-                key={photo.id}
-                src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-                alt={this.props.marker.title+' photo '+i}
-              />
-            ))}
-          </div>
+        <div
+          className="info-window"
+          tabIndex="0">
+            <div><strong>{this.props.marker.title}</strong></div>
+            <div>{this.props.marker.address}</div>
+            <div className="photos">
+              {this.props.photos.map((photo, i) => (
+                <img
+                  key={photo.id}
+                  src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+                  alt={this.props.marker.title+' photo '+i}
+                />
+              ))}
+            </div>
         </div>
       )
     } else {
